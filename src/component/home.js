b621a298ea49a8ad4a519632b5f7df6da2d301cd
Admin <>
2022-07-23 03:57:22 +0800

    ui

--- src/component/home.js
+++ src/component/home.js
@@ -97,16 +97,30 @@ const Home = () => {
           </svg>
         </div>
         <h2 className="text-4xl tracking-tight">
-          {action === "mint" ? "Mint Tokens" : "Transfer Tokens"}
+          {action === "mint" ? "Mint Tokens" : "Deposit LP Token"}
         </h2>
       </div>
       <div className="flex justify-center my-2 mx-4 md:mx-0">
         <form className="w-full max-w-xl bg-white rounded-lg shadow-md p-6">
           <div className="flex flex-wrap -mx-3 mb-6">
             {action === "transfer" && (
+              // <div className="w-full md:w-full px-3 mb-6">
+              //   <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              //     Recipient address
+              //   </label>
+              //   <input
+              //     className="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none"
+              //     type="text"
+              //     onChange={(e) => {
+              //       setAddress(e.target.value);
+              //     }}
+              //     required
+              //   />
+              // </div>
+
               <div className="w-full md:w-full px-3 mb-6">
                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Recipient address
+                  Token Name
                 </label>
                 <input
                   className="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none"
@@ -137,7 +151,7 @@ const Home = () => {
                 onClick={action === "mint" ? mint : transfer}
                 className="block w-full bg-blue-600 text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-blue-500"
               >
-                {action === "mint" ? "Mint" : "Transfer"}
+                {action === "mint" ? "Mint" : "Deposit"}
               </button>
             </div>
             <div className="flex w-screen justify-center items-center">
@@ -149,7 +163,7 @@ const Home = () => {
                 size="large"
                 buttonStyle="solid"
               >
-                <Radio.Button value="transfer">Transfer</Radio.Button>
+                <Radio.Button value="transfer">Deposit LP Token</Radio.Button>
                 <Radio.Button value="mint">Mint</Radio.Button>
               </Radio.Group>
             </div>
